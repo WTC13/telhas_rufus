932fc9904f8cddf00a73bf017d6bf7f98f0ad9f2
Wendell Costa <wendell.timoteoc@gmail.com>
2025-06-17 00:17:02 -0300

    update header-nav

--- src/js/script.js
+++ src/js/script.js
@@ -55,4 +55,40 @@ $(document).ready(function(){
         // Limpa o formulário usando jQuery
         $('#contactForm')[0].reset();
     });
+
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+
+    const sections = $('section');
+    const navTitle = $('.nav-title');
+
+    $(window).on('scroll', function(){
+        const header = $('header');
+        let activeSectionIndex = 0;
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        if(scrollPosition <= 0){
+            header.css('box-shadow', 'none');
+        }
+        else{
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+
+        navTitle.removeClass('active');
+        $(navTitle[activeSectionIndex]).addClass('active');
+    });
 });
