c3032378667fe0a03c2ecc2b0fbd4311b8cf47d2
Wendell Costa <wendell.timoteoc@gmail.com>
2025-06-21 14:11:02 -0300

    update main

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,41 @@
 $(document).ready(function(){
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+
+    const sections = $('section');
+    const navTitle = $('.nav-title');
+
+    $(window).on('scroll', function(){
+        const header = $('header');
+        let activeSectionIndex = 0;
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        if(scrollPosition <= 0){
+            header.css('box-shadow', 'none');
+        }
+        else{
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+
+        navTitle.removeClass('active');
+        $(navTitle[activeSectionIndex]).addClass('active');
+    });
+    
+    
     // --- Configuração do Owl Carousel (seu código original) ---
     $('.owl-carousel').owlCarousel({
         loop:true,
@@ -56,39 +93,5 @@ $(document).ready(function(){
         $('#contactForm')[0].reset();
     });
 
-    $('#mobile_btn').on('click', function() {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
-    });
-
-    const sections = $('section');
-    const navTitle = $('.nav-title');
-
-    $(window).on('scroll', function(){
-        const header = $('header');
-        let activeSectionIndex = 0;
-        const scrollPosition = $(window).scrollTop() - header.outerHeight();
-
-        if(scrollPosition <= 0){
-            header.css('box-shadow', 'none');
-        }
-        else{
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
-        }
-
-        sections.each(function(i) {
-            const section = $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop + section.outerHeight();
-
-            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
-                activeSectionIndex = i;
-                return false;
-            }
-        })
-
-
-        navTitle.removeClass('active');
-        $(navTitle[activeSectionIndex]).addClass('active');
-    });
+    
 });
